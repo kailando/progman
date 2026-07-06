a9b49e68dab22a638eb7d475172bd92dd40bb2a8
kailando <dq11sk@gmail.com>
2026-07-06 18:41:10 -0500

    add favicon

--- server.py
+++ server.py
@@ -1,9 +1,17 @@
-from flask import Flask
+from os.path import join
+from flask import Flask, send_from_directory
 
 app=Flask(__name__)
-
+assets=join(app.root_path, "assets")
 @app.route("/")
 def home():
     return "Test"
 
+@app.route("/favicon.ico")
+def favicon():
+    return send_from_directory(
+        assets,
+        "favicon.png",
+        mimetype="image/png"
+    )
 app.run()
