2aace39bbf0622b0a62d0984ea04fa0a053e82cd
kailando <dq11sk@gmail.com>
2026-07-06 22:14:11 -0500

    Add main page template and update server to render it

--- server.py
+++ server.py
@@ -1,11 +1,11 @@
-from os.path import join
-from flask import Flask, send_from_directory
+from os.path import join, abspath
+from flask import Flask, send_from_directory, render_template
 
-app=Flask(__name__)
+app=Flask(__name__, template_folder=abspath("web"))
 assets=join(app.root_path, "assets")
 @app.route("/")
 def home():
-    return "Test"
+    return render_template("main_page/index.html")
 
 @app.route("/favicon.ico")
 def favicon():
@@ -14,4 +14,5 @@ def favicon():
         "favicon.png",
         mimetype="image/png"
     )
-app.run()
+
+app.run(debug=False, use_reloader=True)
